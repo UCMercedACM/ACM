1d739e2380329f15a35874e20d86e5575a5a2324
Adrian Darian <adarian@ucmerced.edu>
2020-03-19 12:51:08 -0700

    Local Dev Tuolumne working

--- Docker/database_setup.sh
+++ Docker/database_setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# Create members table
-docker exec -it postgres psql -U docker acm -c "create table if not exists members (ID serial primary key not null, student_id varchar(15) not null, first_name varchar(255) not null, last_name varchar(255) not null, email varchar(255) not null, password varchar(255) not null, year varchar(30), github varchar(255), linkedin varchar(255), personal_website varchar(255), stack_overflow varchar(255), portfolium varchar(255), handshake varchar(255), slack varchar(50), discord varchar(50), thumbnail varchar(50), active boolean, banned boolean, privilege varchar(50), created_at TIMESTAMPTZ default NOW());"
+# Create Members Table
+docker exec -it postgres psql -U docker acm -c "create table if not exists members (ID serial primary key not null, student_id varchar(15) not null, first_name varchar(255) not null, last_name varchar(255) not null, email varchar(255) not null, password varchar(255) not null, year varchar(30), github varchar(255), linkedin varchar(255), personal_website varchar(255), stack_overflow varchar(255), portfolium varchar(255), handshake varchar(255), slack varchar(50), discord varchar(50), thumbnail bytea null, active boolean, banned boolean, privilege varchar(50), created_at TIMESTAMPTZ default NOW());"
+
+# Create Workshops Table
+docker exec -it tuolumne php artisan migrate
