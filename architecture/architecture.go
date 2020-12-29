6464acb1ce790655c4bb2733a2da8710f78f414d
Adrian Darian <adarian@ucmerced.edu>
2020-12-28 22:46:20 -0800

    fix: architecture diagram

--- architecture/architecture.go
+++ architecture/architecture.go
@@ -4,66 +4,101 @@ import (
 	"log"
 
 	"github.com/blushft/go-diagrams/diagram"
-	"github.com/blushft/go-diagrams/nodes/alibabacloud"
+	// "github.com/blushft/go-diagrams/nodes/alibabacloud"
 	"github.com/blushft/go-diagrams/nodes/apps"
 	"github.com/blushft/go-diagrams/nodes/aws"
-	"github.com/blushft/go-diagrams/nodes/azure"
-	"github.com/blushft/go-diagrams/nodes/elastic"
-	"github.com/blushft/go-diagrams/nodes/firebase"
+
+	// "github.com/blushft/go-diagrams/nodes/azure"
+	// "github.com/blushft/go-diagrams/nodes/elastic"
+	// "github.com/blushft/go-diagrams/nodes/firebase"
 	"github.com/blushft/go-diagrams/nodes/gcp"
-	"github.com/blushft/go-diagrams/nodes/generic"
-	"github.com/blushft/go-diagrams/nodes/k8s"
-	"github.com/blushft/go-diagrams/nodes/oci"
-	"github.com/blushft/go-diagrams/nodes/openstack"
-	"github.com/blushft/go-diagrams/nodes/outscale"
+	// "github.com/blushft/go-diagrams/nodes/generic"
+	// "github.com/blushft/go-diagrams/nodes/k8s"
+	// "github.com/blushft/go-diagrams/nodes/oci"
+	// "github.com/blushft/go-diagrams/nodes/openstack"
+	// "github.com/blushft/go-diagrams/nodes/outscale"
 	"github.com/blushft/go-diagrams/nodes/programming"
-	"github.com/blushft/go-diagrams/nodes/saas"
+	// "github.com/blushft/go-diagrams/nodes/saas"
 )
 
-func getDNS(name string) *diagram.Node {
-	switch name {
-	case "alibabacloud":
-		return alibabacloud.Web.Dns(diagram.NodeLabel("DNS"))
-	case "gcp":
-		return gcp.Network.Dns(diagram.NodeLabel("DNS"))
-	case "azure":
-		return azure.Network.DnsZones(diagram.NodeLabel("DNS"))
-	case "oci":
-		return oci.Connectivity.Dns(diagram.NodeLabel("DNS"))
-	}
+// Font Struct
+type Font struct {
+	Name  	string
+	Size  	int
+	Color 	string
+}
 
-	return gcp.Network.Dns(diagram.NodeLabel("DNS"))
+// GroupOptions Struct
+type GroupOptions struct {
+	Label           string
+	LabelJustify    string
+	Direction       string
+	PenColor        string
+	BackgroundColor string
+	Shape           string
+	Style           string
+	Font            Font
+	Attributes      map[string]string
 }
 
 func main() {
 	// Configure a new diagram
 	d, err := diagram.New(
-		diagram.Filename("architecture"), 
-		diagram.Label("App"), 
-		diagram.Direction("LR"),
+		diagram.Filename("architecture"),
+		diagram.Label("App"),
+		diagram.Direction("TB"),
 	)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Frontend
 	ng := programming.Framework.Angular(diagram.NodeLabel("Chapter Website"))
 	dns := gcp.Network.Dns(diagram.NodeLabel("DNS"))
-	aws_general := aws.General.General(diagram.NodeLabel("general"))
-	kibana := elastic.Elasticsearch.Kibana(diagram.NodeLabel("Kibana"))
-	firebase := firebase.Base.Firebase(diagram.NodeLabel("Firebase"))
-	blank := generic.Blank.Blank(diagram.NodeLabel("Blank"))
-	helm := k8s.Ecosystem.Helm(diagram.NodeLabel("Helm"))
-	ansible := openstack.Deployment.Ansible(diagram.NodeLabel("Ansible"))
-	net := outscale.Network.Net(diagram.NodeLabel("Net"))
-	slack := saas.Chat.Slack(diagram.NodeLabel("Slack"))
-	docker :=  apps.Container.Docker(diagram.NodeLabel("Docker"))
 
-	dc := diagram.NewGroup("Test").Add(
-		ng, dns, aws_general, kibana, firebase, blank, helm, ansible, net, slack, docker,
+	// Backend APIs
+	halfDome := programming.Language.Nodejs(diagram.NodeLabel("Half Dome"))
+	tuolumne := programming.Framework.Laravel(diagram.NodeLabel("Tuolumne"))
+	tenaya := programming.Language.Go(diagram.NodeLabel("Tenaya"))
+	mariposa := gcp.Devtools.MavenAppEnginePlugin(diagram.NodeLabel("Mariposa"))
+	
+	// DevOps
+	apiGateway := aws.Network.ApiGateway(diagram.NodeLabel("Api Gateway"))
+
+	// defaultOpts := diagram.DefaultGroupOptions()
+	// apiOpts := diagram.GroupOption(defaultOpts)
+
+	api := diagram.NewGroup("API").Label("API Layer")
+	api.NewGroup("Half Dome").Label("Service 1").Add(
+		halfDome,
+		apps.Database.Postgresql(diagram.NodeLabel("Replica")), 
+		apps.Container.Docker(diagram.NodeLabel("Docker")),
 	)
+	api.NewGroup("Tuolumne").Label("Service 2").Add(
+		tuolumne,
+		apps.Database.Postgresql(diagram.NodeLabel("Replica")), 
+		apps.Container.Docker(diagram.NodeLabel("Docker")),
+	)
+	api.NewGroup("Tenaya").Label("Service 3").Add(
+		tenaya,
+		apps.Database.Postgresql(diagram.NodeLabel("Replica")), 
+		apps.Container.Docker(diagram.NodeLabel("Docker")),
+	)
+	api.NewGroup("Mariposa").Label("Service 4").Add(
+		mariposa,
+		apps.Database.Postgresql(diagram.NodeLabel("Replica")), 
+		apps.Container.Docker(diagram.NodeLabel("Docker")),
+	)
+	api.ConnectAllFrom(apiGateway.ID(), diagram.Forward())
 
-	d.Group(dc)
+	d.Connect(dns, ng, diagram.Forward())
+	d.Connect(ng, apiGateway, diagram.Forward())
+	d.Connect(apiGateway, halfDome, diagram.Forward())
+	d.Connect(apiGateway, tuolumne, diagram.Forward())
+	d.Connect(apiGateway, tenaya, diagram.Forward())
+	d.Connect(apiGateway, mariposa, diagram.Forward())
+	d.Group(api)
 
 	if err := d.Render(); err != nil {
 		log.Fatal(err)
